acc44ef86cae68d5194fbd07c195de5fc75b86f0
GDA <gulyaev@experium.ru>
2022-07-20 13:34:06 +0300

    simpify templates

--- convert.h
+++ convert.h
@@ -14,30 +14,18 @@ namespace conv
 	int extractId(std::string_view sw);
 
 	void unpack_goods_str(Entities::GoodsPack&, std::string_view sw);
-	
-	// default
-	template<typename Ret>
-	static void from_string(Ret& ret, std::string_view value)
-	{
-		static_assert(false, "oups! do specialize " __FUNCTION__);
-		auto n = typeid(ret).name();
-		int i = 8;
-	};
 
-	template<>
 	static void from_string(Entities::GoodsQty& ret, std::string_view value)
 	{
 		unpack_goods_str(ret.packed, value);
 	}
 
-	template<>
 	static void from_string(Entities::GoodsPrice& ret, std::string_view value)
 	{
 		unpack_goods_str(ret.packed, value);
 	}
 
 	// for int, double
-	template<>
 	static void from_string(int& ret, std::string_view value)
 	{
 		std::from_chars_result err =
@@ -47,7 +35,6 @@ namespace conv
 			throw std::logic_error(__FUNCTION__ " err!");
 	}
 
-	template<>
 	static void from_string(double& ret, std::string_view value)
 	{
 		std::from_chars_result err =
@@ -57,33 +44,17 @@ namespace conv
 			throw std::logic_error(__FUNCTION__ " err!");
 	}
 
-	template<>
 	static void from_string(std::string& ret, std::string_view value)
 	{
 		ret = {value.data(), value.data() + value.size()};
 	}
 
-	template<>
 	static void from_string(Entities::Type& ret, std::string_view value)
 	{
 		// fix it. has same functionality!
 		ret = model::converter<Entities::Type>().from_string(value);
 	}
-
-	template<typename T>
-	static void from_string(std::optional<T>& ret, std::string_view value)
-	{
-		if(value.empty()) 
-			ret = {};
-		else 
-		{
-			T t;
-			from_string(t, value);
-			ret = { t };
-		}
-	}
 	
-	template<>
 	static void from_string(bool& ret, std::string_view value)
 	{
 		if(value == "False")
@@ -95,6 +66,30 @@ namespace conv
 			throw std::logic_error(__FUNCTION__ " err!");
 		}
 	}
+
+	// insert from_string specializations here =>
+
+	template<typename T>
+	static void from_string(std::optional<T>& ret, std::string_view value)
+	{
+		if (value.empty())
+			ret = {};
+		else
+		{
+			T t {};
+			from_string(t, value);
+			ret = { t };
+		}
+	}
+
+	//// default
+	//template<typename Ret>
+	//static void from_string(Ret& ret, std::string_view value)
+	//{
+	//	static_assert(false, "oups! do specialize " __FUNCTION__);
+	//	auto n = typeid(ret).name();
+	//	int i = 8;
+	//};
 
 	template<typename T, typename Ret, typename Base,
 		typename SFINAE = std::enable_if_t<std::is_base_of_v<T, Base>>
