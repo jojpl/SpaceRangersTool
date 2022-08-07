18c2efd771b93c21d199d3917e9022dfebac1f01
GDA <gulyaev@experium.ru>
2022-08-07 20:17:59 +0300

    GoodsPack from_string rewrtite

--- convert.cpp
+++ convert.cpp
@@ -2,9 +2,7 @@
 
 #include <boost/algorithm/string/split.hpp>
 #include <array>
-#include <functional>
 #include <cctype>
-#include <limits>
 
 namespace conv
 {
@@ -24,22 +22,35 @@ namespace conv
 		return i;
 	}
 
-	void from_string(Entities::GoodsPack& packed, std::string_view sw)
+	std::vector<std::string_view> unpack(std::string_view sw)
 	{
-		std::vector<boost::iterator_range<std::string_view::iterator>> splitVec;
+		using sw_iters_range = boost::iterator_range<std::string_view::iterator>;
+		
+		std::vector<sw_iters_range> splitVec;
 		boost::split(splitVec, sw, std::bind(std::equal_to<char>(), _1, ','));
 
-
-		int iter_enum = (int)Entities::GoodsEnum {};
-		auto iter_range = splitVec.cbegin();
-		
-		for (;  iter_enum != ENUM_COUNT(Entities::GoodsEnum) && iter_range!= splitVec.cend();
-				iter_enum++, iter_range++)
+		std::vector<std::string_view> vsw;
+		for (auto iter_range : splitVec)
 		{
-			auto rng = *iter_range;
+			const char* sw_b = &*iter_range.begin();
+			vsw.emplace_back(sw_b, iter_range.size());
+		}
+		return vsw;
+	}
+
+	void from_string(Entities::GoodsPack& packed, std::string_view sw)
+	{
+		auto un = unpack(sw);
+		const auto& enums = model::enums::get_enums<Entities::GoodsEnum>();
+		if(un.size()!= packed.size()) throw std::logic_error(__FUNCTION__);
+
+		auto iter_enum = enums.cbegin();
+		for (auto g_sw: un)
+		{
 			int res = 0;
-			from_string(res, { &*rng.begin(), rng.size() });
-			packed[iter_enum] = res;
+			from_string(res, g_sw);
+
+			packed[(int)*iter_enum++] = res;
 		}
 	}
 
